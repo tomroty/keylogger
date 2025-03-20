09d59f9ec57d048945cf892340e086950f7f0b43
tomroty <tom.roty@proton.me>
2025-03-20 14:24:59 +0100

    first commit

---
+++ keylogger.py
@@ -0,0 +1,11 @@
+from pynput import keyboard
+
+def on_press(key):
+    try:
+        print('key {0} pressed'.format(key.char))
+    except AttributeError:
+        print('special key {0} pressed'.format(key))
+        
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
+
